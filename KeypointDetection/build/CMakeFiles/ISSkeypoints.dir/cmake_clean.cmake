90feddc531869b6a69ba04874255683cc4e6961e
yveszimmermann <yvesz@ethz.ch>
2016-03-19 16:49:52 +0100

    Initial Commit!

---
+++ KeypointDetection/build/CMakeFiles/ISSkeypoints.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ISSkeypoints.dir/ISSkeypoints.cpp.o"
+  "ISSkeypoints.pdb"
+  "ISSkeypoints"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ISSkeypoints.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
